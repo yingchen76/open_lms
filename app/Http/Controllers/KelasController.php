540a9a4a91a59763ec38643a23abfdaf744ffdee
yingchen76 <cying153@gmail.com>
2019-06-14 23:04:29 +0700

    deleted all

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -104,14 +104,14 @@ class KelasController extends Controller
         //
 
         $kelas = Kelas::find($id);
-        $murid = Murid::where('kelas_id',$id)->get();   
-        $post = post::where('kelas_id',$id)->get();   
-        $tugas = Tugas::where('kelas_id',$id)->get();   
-        // $kelas->delete();
-        // $murid->delete();  
-        // $post->delete();  
-        // $tugas->delete();  
-        return $murid;
-        // return redirect('kelas');
+        $kelas->delete();
+        $murid = Murid::where('kelas_id',$id);   
+        $murid->delete();  
+        $post = post::where('kelas_id',$id);
+        $post->delete();  
+        $tugas = Tugas::where('kelas_id',$id);
+        $tugas->delete();  
+        // return $murid;
+        return redirect('kelas');
     }
 }
